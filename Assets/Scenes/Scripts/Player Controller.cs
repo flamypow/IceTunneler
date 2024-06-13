4fd72611789f4a5cf314591ee2232824f42d0cff
JPatel7171 <j.patel717@mybvc.ca>
2024-06-14 03:51:48 +0530

    added player controller but my player was flying in the air

---
+++ Assets/Scenes/Scripts/Player Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    float vertical, horizontal;
+
+    Rigidbody2D myRigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        myRigidbody2D.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
